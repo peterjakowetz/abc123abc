e75fa55a2d96ea9a83ca3cc94d99f60bf2608d13
Tim Hobbs <xika486@gmail.com>
2011-09-14 02:28:15 +1200

    more logic lwsdjifhiusjdhge

--- logic.h
+++ logic.h
@@ -8,9 +8,28 @@ void doLogicStep (Bot* bot, unsigned short timeDeltaMillis)
     if (bot->leftSensor == GREY || bot->rightSensor == GREY || bot->centreSensor == GREY) bot->done = 1;
 	else  if (bot->leftSensor == WHITE)
 	{
-	  bot->leftSpeed = 127;
-	  bot->rightSpeed = 36;
-	  bot->checkdelay = 100;
+	  if (bot->centreSensor == WHITE)
+	  {
+		bot->leftSpeed = 127;
+		bot->rightSpeed = 0;
+		bot->checkdelay = 100;
+	  }
+	  else if( bot->centreSensor == BLACK && bot->rightSensor == WHITE)
+	  {
+		bot->leftSpeed = 127;
+		bot->rightSpeed = 127;
+		bot->checkdelay = 100;
+	  }
+	  else if( bot->centreSensor == BLACK && bot->rightSensor == BLACK)
+	  {
+		bot->leftSpeed = 127;
+		bot->rightSpeed = -127;
+		bot->checkdelay = 100;
+	  }
+	  
+// 	  bot->leftSpeed = 127;
+// 	  bot->rightSpeed = 36;
+// 	  bot->checkdelay = 100;
 	}
 // 		if (bot->centreSensor == BLACK && bot->rightSensor == WHITE)
 // 	    {
@@ -41,7 +60,7 @@ void doLogicStep (Bot* bot, unsigned short timeDeltaMillis)
 	  {
 		bot->leftSpeed = -127;
 		bot->rightSpeed = 127;
-		bot->checkdelay = 500;
+		bot->checkdelay = 400;
 	  }
 	}
   
